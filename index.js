efb2c77c12878ce2dfffc382d7a3423ce729f473
cynthia <cyeeyearn@gmail.com>
2024-03-30 23:03:37 +0300

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Test cases
+console.log(saturdayFun()); 
+console.log(saturdayFun("bathe my dog")); 
+
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Test cases
+console.log(mondayWork()); 
+console.log(mondayWork("work from home")); 
+
+//
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction()); 
+console.log(encouragingPromptFunction("amazing")); 
+
